ff58da55505e9614edc9fd81a8c04e92956d8ac9
gyaresu <me@gareth.com.au>
2014-04-13 21:31:07 +0200

    flatten nested arrays

--- flatten_netsted_arrays.js
+++ flatten_netsted_arrays.js
@@ -1,15 +1,28 @@
-var flatten = function (array) {
+function flatten(array) {
   var result = [];
-  if (array.length <= 1)
-    return array;
-  for (var i = 0; i < array.length; i++)
-    for (var j = 0; j < array.length; j++)
-      result.push(array[i][j]);
+  array.forEach(function (item) {
+    if (!Array.isArray(item))
+      result.push(item);
+    else 
+      for (var i = 0; i < item.length; i++)
+        result.push(item[i]);
+  });
   return result;
-};
+}
 
-console.log(flatten([[1,2,3],["a","b","c"],[1,2,3]]));
-console.log("This?: " + flatten([[[1,2,3]]])); // => [
 console.log(flatten([1,2,3])); // => [1,2,3]
 console.log(flatten([[1,2,3],["a","b","c"],[1,2,3]]));  // => [1,2,3,"a","b","c",1,2,3]
 console.log(flatten([[[1,2,3]]])); // => [[1,2,3]]
+
+// Top solutions:
+//
+// var flatten = function (lol){
+//   return [].concat.apply([],lol);
+//   }
+//
+// ---
+//
+// var flatten = Function.prototype.apply.bind(Array.prototype.concat, []);
+//
+// ---
+//
